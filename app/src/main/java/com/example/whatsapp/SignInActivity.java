f58efde41337d2b6ca5373aaa5fc068315940bdf
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-11-19 23:45:51 +0530

    added sign In activity direct access

--- app/src/main/java/com/example/whatsapp/SignInActivity.java
+++ app/src/main/java/com/example/whatsapp/SignInActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.whatsapp.databinding.ActivitySignInBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -35,9 +37,27 @@ public class SignInActivity extends AppCompatActivity {
                 auth.signInWithEmailAndPassword(binding.etEmail.getText().toString(), binding.etPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(SignInActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
-                })
+                });
+                binding.tvClickSignUp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                if(auth.getCurrentUser()!=null){
+                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
