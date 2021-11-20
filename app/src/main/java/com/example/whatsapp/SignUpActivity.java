e1e83430c2785cd25cc1ef8a51969e5e290fff3a
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-11-20 14:27:59 +0530

    added google sign in option

--- app/src/main/java/com/example/whatsapp/SignUpActivity.java
+++ app/src/main/java/com/example/whatsapp/SignUpActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -27,6 +28,8 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
 
         getSupportActionBar().hide();
         mAuth = FirebaseAuth.getInstance();
@@ -55,6 +58,13 @@ public class SignUpActivity extends AppCompatActivity {
                     }
                 });
 
+            }
+        });
+        binding.tvAlreadyAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
             }
         });
 
